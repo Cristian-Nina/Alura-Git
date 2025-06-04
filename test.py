20741de5ea4e831c04b2bd28085747681332c4e6
Cristian Nina <110742774+Cristian-Nina@users.noreply.github.com>
2025-06-04 17:40:44 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 import certstream
 
 def callback(message, context):
-    if message["message_type"] != "certificate_update":
-        return
+    print(message)  # Muestra todo el JSON recibido
 
-    dominios = message["data"]["leaf_cert"]["all_domains"]
-    if dominios:
-        print(f"[+] Dominio detectado: {dominios[0]}")
-
-print("Escuchando certificados desde Certstream...\n")
 certstream.listen_for_events(callback, url='wss://certstream.calidog.io/')
